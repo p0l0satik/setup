b67e9b116059a78dc345b4f79455caeed37b3f77
p0l0satik <p0l0satik@MacBook-Pro-p0l0satik.local>
2019-10-17 12:04:25 +0300

    added init script vundle init git name/email configuration

---
+++ init.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ./.vimrc ~/.vimrc
+
+git config --global user.email "denis.cucu@yandex.ru"
+git config --global user.name "p0l0satik"
+
