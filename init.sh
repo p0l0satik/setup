ecc19c2c5db09d3d86921e2525b29342a880275d
p0l0satik <denis.cucu@yandex.ru>
2019-11-05 08:46:08 +0300

    term colors?

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
+cp ./.bashrc ~/.bashrc
+source ~/.bashrc
 
 git config --global user.email "denis.cucu@yandex.ru"
 git config --global user.name "p0l0satik"
